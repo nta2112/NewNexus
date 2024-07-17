d25cfc6dfb8adec949a010507fa6a7b41e0f53ce
nta2112 <ann249955@gmail.com>
2024-07-17 09:26:17 +0700

    Add project files.

---
+++ Nexus/Controllers/Admin/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nexus.Models;
+
+
+namespace Nexus.Controllers.Admin
+{
+    public class AdminController : Controller
+    {
+        private readonly NexusContext _context;
+        public AdminController(NexusContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+		public IActionResult TbEmployee()
+		{
+			var employees = _context.Employees.ToList();
+
+			return View(employees);
+		}
+	}
+}
---
+++ Nexus/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nexus.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public int RoleId { get; set; }
+
+    public int ShopId { get; set; }
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual RetailShop Shop { get; set; } = null!;
+}
