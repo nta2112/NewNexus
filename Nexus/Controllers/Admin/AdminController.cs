f6301382f61e8df8c4cc5166d3099d067633da99
nta2112 <ann249955@gmail.com>
2024-07-20 11:07:04 +0700

    add function succeed

--- Nexus/Controllers/Admin/AdminController.cs
+++ Nexus/Controllers/Admin/AdminController.cs
@@ -88,5 +88,65 @@ namespace Nexus.Controllers.Admin
             }
         }
 
+        public IActionResult DelEmployee(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(e => e.EmployeeId == id);
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(TbEmployee));
+        }
+
+        public IActionResult AddEmployee()
+        {
+            ViewBag.RoleId = new SelectList(_context.Roles, "RoleId", "RoleName");
+            ViewBag.ShopId = new SelectList(_context.RetailShops, "ShopId", "Address");
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult AddEmployee(Employee employee)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Employees.Add(employee);
+        //        _context.SaveChanges();
+        //        return RedirectToAction(nameof(TbEmployee));
+        //    }
+        //    ViewBag.RoleId = new SelectList(_context.Roles, "RoleId", "RoleName", employee.RoleId);
+        //    ViewBag.ShopId = new SelectList(_context.RetailShops, "ShopId", "ShopName", employee.ShopId);
+        //    return View(employee);
+        //}
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddEmployee(IFormCollection collection)
+        {
+            try
+            {
+                using (var db = new NexusContext())
+                {
+                    var employee = new Employee
+                    {
+                        Name = collection["Name"],
+                        Email = collection["Email"],
+                        Password = collection["Password"],
+                        RoleId = Convert.ToInt32(collection["RoleId"]),
+                        ShopId = Convert.ToInt32(collection["ShopId"])
+                    };
+
+                    db.Employees.Add(employee);
+                    db.SaveChanges();
+                }
+                return RedirectToAction(nameof(Index)); 
+            }
+            catch
+            {
+                ViewBag.RoleId = new SelectList(_context.Roles, "RoleId", "RoleName");
+                ViewBag.ShopId = new SelectList(_context.RetailShops, "ShopId", "Address");
+                return View();
+            }
+        }
     }
 }
--- Nexus/Controllers/HomeController.cs
+++ Nexus/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Nexus.Models;
 using System.Diagnostics;
 
@@ -6,14 +7,19 @@ namespace Nexus.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        
+        private readonly NexusContext _context;
+        public HomeController(NexusContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
         {
             return View();
         }
@@ -28,5 +34,43 @@ namespace Nexus.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                ModelState.AddModelError(string.Empty, "Email and password are required.");
+                return View();
+            }
+
+           
+            var user = await _context.Employees
+                                     .FirstOrDefaultAsync(e => e.Email == email && e.Password == password);
+
+            if (user != null)
+            {
+                
+                switch (user.RoleId)
+                {
+                    case 1:
+                        return RedirectToAction("Index", "Admin");
+                    case 2: 
+                        return RedirectToAction("Index", "Technical");
+                    default:
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        break;
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            }
+
+            return View();
+        }
+
     }
 }
